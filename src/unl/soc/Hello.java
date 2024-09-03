1b217d73c7ee354ee05b1e1c4ae5246bcb0dbe9b
oshan <oshan@DESKTOP-Q2A5IG4.unl.edu>
2024-09-02 20:18:47 -0500

    My commit to Ermias' project (My name and major)

--- src/unl/soc/Hello.java
+++ src/unl/soc/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String args) {
 		System.out.println("Ermias Wolde");
 		System.out.println("Computer Science");
+		System.out.println("Oscar Hanson");
+		System.out.println("Computer Science");
 	}
 
 }
